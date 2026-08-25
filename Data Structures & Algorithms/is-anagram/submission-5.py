529b441e4d77fced3b48bd71115a6b8c6f006db4
TravisChen77 <travischen2024@gmail.com>
2026-08-25 14:30:18 -0600

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        word1 = {}
+        word2 = {}
+        for char in s:
+            if char in word1:
+                word1[char]+= 1
+            else:
+                word1[char] = 1
+        for char in t:
+            if char in word2:
+                word2[char]+= 1
+            else:
+                word2[char] = 1
+
+        if word1 == word2:
+            return True
+        else:
+            return False
